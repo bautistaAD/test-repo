e33de77c4c4dbbee75d22d5f224ef699e05272e9
ambautista <adriennemae10@gmail.com>
2026-03-09 09:05:27 +0800

    Add comments

--- count.py
+++ count.py
@@ -1,11 +1,11 @@
 i = 10
 
-for i in range(10):
+for i in range(10): #this will print 0-9
   print(i)
 
 
 sum = 0
-for i in range(10):
+for i in range(10): # this will add 0-9
   sum += i
 
 print("SUM: ", sum)
